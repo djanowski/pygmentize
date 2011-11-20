64a44e25280347a04bd26fdb3550636d39b0184a
Andrew Vos <andrew.vos@gmail.com>
2011-11-20 12:25:02 +0000

    Cache the response from pygments to speed things up a bit

--- lib/pygmentize.rb
+++ lib/pygmentize.rb
@@ -14,10 +14,21 @@ class Pygmentize
       "-O", "encoding=#{source.encoding}"
     ]
 
-    IO.popen("#{bin} #{Shellwords.shelljoin args}", "r+") do |io|
-      io.write(source)
-      io.close_write
-      io.read
+    key = create_cache_key(source, lexer, args)
+    @cached_results ||= {}
+
+    unless @cached_results[key]
+      @cached_results[key] = IO.popen("#{bin} #{Shellwords.shelljoin args}", "r+") do |io|
+        io.write(source)
+        io.close_write
+        io.read
+      end
     end
+    @cached_results[key]
+  end
+
+  private
+  def self.create_cache_key(source, lexer, args = [])
+    "#{source}#{lexer}#{args.join}"
   end
 end
