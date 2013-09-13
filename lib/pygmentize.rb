b52d4cd1fa54c21f3f67edc7a01e1f4ba81cb3cb
Eric Wood <ericw@spiceworks.com>
2013-09-12 22:01:35 -0500

    Support for multiple options through hash syntax.

--- lib/pygmentize.rb
+++ lib/pygmentize.rb
@@ -1,20 +1,25 @@
 require "shellwords"
 
 class Pygmentize
-  VERSION = "0.0.3"
+  VERSION = "0.0.3.1"
 
   def self.bin
     "/usr/bin/env python #{File.expand_path("../vendor/pygmentize.py", File.dirname(__FILE__))}"
   end
 
-  def self.process(source, lexer, args = [])
-    args += [
+  def self.process(source, lexer, options)
+    options[:encoding] = source.encoding
+    opts_string = options.to_a.reduce([]) { |a,i|
+      a << "-O #{i.map { |i| Shellwords.escape(i.to_s) }.join('=')}"
+    }.join(' ')
+
+    args = [
       "-l", lexer.to_s,
-      "-f", "html",
-      "-O", "encoding=#{source.encoding}"
+      "-f", "html"
     ]
 
-    IO.popen("#{bin} #{Shellwords.shelljoin args}", "r+") do |io|
+    cmd = "#{bin} #{Shellwords.shelljoin args} #{opts_string}"
+    IO.popen(cmd, "r+") do |io|
       io.write(source)
       io.close_write
       io.read
