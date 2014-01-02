58b923066713abcf967a8836a7177193e64f96a3
Eric Wood <ericw@spiceworks.com>
2014-01-02 11:44:39 -0600

    Tests for CLI options functionality
    Options generator is now in its own function for ease in testing.

--- lib/pygmentize.rb
+++ lib/pygmentize.rb
@@ -7,18 +7,24 @@ class Pygmentize
     "/usr/bin/env python #{File.expand_path("../vendor/pygments/pygmentize", File.dirname(__FILE__))}"
   end
 
-  def self.process(source, lexer, options = {})
-    options[:encoding] = source.encoding
+  # generate string of additional options from hash
+  def self.options_string(options)
     opts_string = options.to_a.reduce([]) { |a,i|
       a << "-O #{i.map { |i| Shellwords.escape(i.to_s) }.join('=')}"
     }.join(' ')
+
+    opts_string
+  end
+
+  def self.process(source, lexer, options = {})
+    options[:encoding] = source.encoding
 
     args = [
       "-l", lexer.to_s,
       "-f", "html"
     ]
 
-    cmd = "#{bin} #{Shellwords.shelljoin args} #{opts_string}"
+    cmd = "#{bin} #{Shellwords.shelljoin args} #{options_string(options)}"
     IO.popen(cmd, "r+") do |io|
       io.write(source)
       io.close_write
--- test/pygmentize.rb
+++ test/pygmentize.rb
@@ -2,6 +2,7 @@
 
 require File.expand_path("../lib/pygmentize", File.dirname(__FILE__))
 require "nokogiri"
+require "shellwords"
 
 test "colorizes output" do
   output = Pygmentize.process(%Q[{foo: "bar"}], :javascript)
@@ -17,3 +18,16 @@ test "handles encodings" do
 
   assert output.encoding == Encoding::UTF_8
 end
+
+test "handles command line options" do
+  to_escape = "two words"
+  options = {foo: "bar", bar: to_escape}
+
+  opts = Pygmentize.options_string(options)
+
+  assert opts.include?("-O foo=bar ")
+
+  # ensure escaping of options
+  assert opts.include?("-O bar=#{Shellwords.escape(to_escape)}")
+end
+
